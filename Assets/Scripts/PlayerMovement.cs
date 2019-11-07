9a432cfe1b9cb29e34c46cecb52b786940df610c
KevinKCD <kevinkcdoan@gmail.com>
2019-11-07 03:49:41 +0000

    First Commit
    Commit with player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Animator animator;
+    public float runSpeed = 10f;
+    private float JumpForce = 5f;
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+    bool attack = false;
+    private bool m_FacingRight = true;  // For determining which way the player is currently facing.
+
+    private bool isGrounded;            // Whether or not the player is grounded.
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+    private int JumpCount = 0;
+    public int doubleJump;
+
+    Rigidbody2D rb;
+
+  
+    void Start()
+    {
+        doubleJump = JumpCount;
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("Jumping", true);
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+            animator.SetBool("Crouching", true);
+
+        }
+        if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+            animator.SetBool("Crouching", false);
+
+        }
+        if (Input.GetButtonDown("Attack"))
+        {
+            attack = true;
+            animator.SetBool("Attacking", true);
+        }
+        if (Input.GetButtonUp("Attack"))
+        {
+            attack = false;
+            animator.SetBool("Attacking", false);
+
+        }
+
+        rb.velocity = new Vector2(horizontalMove * runSpeed, rb.velocity.y);
+        if (horizontalMove > 0 && !m_FacingRight)
+        {
+            // ... flip the player.
+            Flip();
+        }
+        // Otherwise if the input is moving the player left and the player is facing right...
+        else if (horizontalMove < 0 && m_FacingRight)
+        {
+            // ... flip the player.
+            Flip();
+        }
+
+     
+        if (isGrounded == true)
+        {
+            doubleJump = JumpCount;
+        }
+        if (jump && (JumpCount < 1) && isGrounded)
+        {
+            Debug.Log(JumpCount);
+            rb.velocity = Vector2.up * JumpForce;
+            ++JumpCount;
+        }
+    }
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+    }
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        m_FacingRight = !m_FacingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
